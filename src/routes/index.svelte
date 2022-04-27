2a34a2386ebb362735dee275e092af69c738969e
David Rio Deiros <driodeiros@gmail.com>
2022-04-27 13:54:36 -0400

    checkpoint

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,64 +3,38 @@
   import { simulateDelay, genRandomItem } from "$lib/utils.ts";
   import { deleteItem, getItems } from "$lib/api.ts";
   import { itemStore } from "$lib/stores.ts";
-  import { onMount } from "svelte";
+  import { onMount, onDestroy } from "svelte";
   import { fade } from "svelte/transition";
   import { goto } from "$app/navigation";
-
-  function hideAndGo(path) {
-    hideContainer = true;
-    goto(path);
-  }
-
-  /*
-  async function handleRandomAdd() {
-    loading = true;
-    const fx = () => itemStore.set([...$itemStore, genRandomItem()]);
-    simulateDelay(fx, 100).then(() => (loading = false));
-  }
-  */
 
   async function handleDelete(idToDelete) {
     const fx = $itemStore.filter((i) => i.id !== idToDelete);
     deleteItem(idToDelete).then(() => itemStore.set(fx));
   }
 
-  let loading = false;
-  let hideContainer = false;
-
   onMount(() => {
     getItems().then(({ items }) => itemStore.set(items));
   });
 </script>
 
-<div class:hide-container={hideContainer}>
+<div class>
   <p class="title">(List of items)</p>
 
   <div class="buttons">
-    <button on:click={() => hideAndGo("/edit?id=0")}>new</button>
-    <!--<button on:click={handleRandomAdd}> Random Add </button>-->
-    <span class="loading" style:visibility={loading ? "visible" : "hidden"}>
-      🐢 ...
-    </span>
+    <button on:click={() => goto("/edit?id=0")}>new</button>
   </div>
 
-  <div class="list-main-container" class:hide-container={hideContainer}>
+  <div class="list-main-container">
     {#if $itemStore.length > 0}
       {#each $itemStore as item (item)}
-        <div
-          class="entry"
-          in:fade={{ duration: 400 }}
-          out:fade={{ duration: 400 }}
-        >
+        <div class="entry">
           <div class="entry-data">
             <span class="id">{item.id}</span>
             <span class="name">{item.name}</span>
             <span class="age">{item.age}</span>
           </div>
           <div class="entry-links">
-            <a href="#" on:click={() => hideAndGo(`/edit?id=${item.id}`)}
-              >edit</a
-            >
+            <a href="#" on:click={() => goto(`/edit?id=${item.id}`)}>edit</a>
             <a href="#" on:click={() => handleDelete(item.id)}>delete</a>
           </div>
         </div>
@@ -70,15 +44,6 @@
 </div>
 
 <style>
-  .loading {
-    font-size: 1.5rem;
-  }
-  .hide-container {
-    position: absolute;
-    top: -1000px;
-    border: color 1px red;
-  }
-
   .buttons {
     padding-bottom: 10px;
   }
